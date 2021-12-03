c3a316c039c2187e100982b80fffe9e2ad86c6db
Pawankumar7814 <pawank4285@gmail.com>
2021-12-03 21:20:29 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+ //All require modules
+ let express = require('express');
+ let http = require('http');
+ const ejs = require('ejs');
+
+ //creating application
+ let app = express();
+
+ //view engine
+ app.set('view engine', 'ejs');
+ //assign port number
+ let port = 3600 | process.env.port;
+
+ app.use("/img", express.static(__dirname + 'Public/Images'));
+
+ //My routes
+ app.get('/resume', (req, res) => {
+     res.render('../views/index.ejs');
+ })
+
+ //Start http server
+ http.createServer(app).listen(port, () => {
+     console.log("Port number is " + port);
+ })
+
+
+ //1) npm init 2) npm i express 3) node app.js(FILE NAME) 4)
